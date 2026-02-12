be638ab6bf98af9b574ac21e868311057214cf85
Marcelo Paschoareli De Moura Barros <marcelobarros-ieg@instituto.ad>
2026-02-12 09:30:06 -0300

    components

--- src/pages/Professor/Professor.jsx
+++ src/pages/Professor/Professor.jsx
@@ -1,31 +1,63 @@
-import BarraSuperior from "../../Components/BarraSuperior/BarraSuperior"
-import Foto from "../../../public/assets/foto_perfil.svg"
 import Filtro from "../../../public/assets/filter_icon.svg"
 import CardAluno from "../../Components/CardAluno/CardAluno"
 import "./Professor.css"
+import { useState } from "react"
+import SideBar from "../../Components/SideBar/SideBar"
 
 function Professor() {
 
-    const alunos = [
-        { id:1, nome:"Marcelo Paschoareli", matricula:"998900", status:"Em Revisão"},
-        { id:1, nome:"Marcelo Paschoareli", matricula:"998900", status:"Em Revisão"},
-        { id:1, nome:"Marcelo Paschoareli", matricula:"998900", status:"Em Revisão"},
-    ]
+    const [matricula, setMatricula] = useState("")
+    const [termoFiltrado, setTermoFiltrado] = useState("");
+    const [ordem, setOrdem] = useState(0);
+    const [mostrarDropdown, setMostrarDropdown] = useState(false);
+
+
+    const [alunos, setAlunos] = useState([        
+        { id:1, nome:"Arcelo Paschoareli", matricula:"998900", status:"Em Revisão"},
+        { id:2, nome:"Barcelo Paschoareli", matricula:"124302", status:"Em Revisão"},
+        { id:3, nome:"Carcelo Paschoareli", matricula:"3413440", status:"Em Revisão"},
+                { id:4, nome:"Darcelo Paschoareli", matricula:"3413440", status:"Em Revisão"},
+                        { id:5, nome:"Earcelo Paschoareli", matricula:"3413440", status:"Em Revisão"},
+
+
+    ]);
+
+    const handleBuscar = () => {
+        setTermoFiltrado(matricula);
+        console.log(ordem)
+    };    
 
     return (
         <div className="professor-body">
-            <BarraSuperior nome={"Professor Paulo"} foto={Foto}></BarraSuperior>
-            <div id="professor-content">
+            <SideBar></SideBar>
+                <div id="professor-content">
                 <div id="professor-search">
-                    <input type="text" placeholder="Buscar por Matrícula" id="search-input"/>
-                    <button id="buttom-filtrar">
-                        <img src={Filtro} width="20vh" />
-                    </button>
-                    <button id="buttom-buscar">Buscar</button>
+                    <input type="text" placeholder="Buscar por Matrícula" id="search-input" onChange={(e) => {setMatricula(e.target.value); if(e.target.value === "") setTermoFiltrado("");}}/>
+                    <div>
+                    <button
+            id="buttom-filtrar"
+            onClick={() => setMostrarDropdown(!mostrarDropdown)}
+            >
+            <img src={Filtro} width="20vh" />
+            </button>
+            <div id="dropdown-content" className={mostrarDropdown ? "ativo" : ""}>
+                    <button id="drop-element" onClick={() => {setOrdem(1); console.log(ordem)}}>A-Z</button>
+                    <button id="drop-element" onClick={() => setOrdem(-1)}>Z-A</button>
+                </div>
+            </div>
+            <button id="buttom-buscar" onClick={handleBuscar}>Buscar</button>
                 </div>
                 <div id="professor-alunos">
                     {
-                      alunos.map((aluno) => (
+                      [...alunos]
+                      .filter(aluno => aluno.matricula.includes(termoFiltrado))
+                      .sort((a, b) =>
+                        ordem === 1
+                          ? a.nome.localeCompare(b.nome)
+                          : ordem === -1
+                          ? b.nome.localeCompare(a.nome)
+                          : 0
+                      ).map((aluno, index) => (
                         <CardAluno
                             id={aluno.id}
                             foto={aluno.foto}
