d40df73eae1b24eea1e106a62d248a7580f1efa1
Marcelo Paschoareli De Moura Barros <marcelobarros-ieg@instituto.ad>
2026-02-16 18:17:50 -0300

    feature: criando a sidebar e arrumando a tela de lançar notas

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,21 +1,44 @@
+import { useState } from "react"
 import styles from "./SideBar.module.css"
 import Logo from "../../../public/assets/logo_side.svg"
 import Desempenho from "../../../public/assets/desempenho_icon.svg"
+import Lancar from "../../../public/assets/lancar_icon.svg"
+import Recado from "../../../public/assets/recado_icon.svg"
 
+function SideBar({ aba, escolher }) {
 
-function SideBar() {
+    const clicar = (escolha) => {
+        escolher(escolha)
+    }
+
     return (
         <div className={styles.barContent}>
-            <img src={Logo} width="180vh"/>
+            <img src={Logo} id={styles.imagem}/>
+
             <div id={styles.navegadores}>
-                <div id={styles.desempenho}>
-                    <img src={Desempenho} width="16vh"/>
+                <div
+                    className={`${styles.opcao} ${aba === "desempenho" ? styles.ativo : ""}`}
+                    onClick={() => clicar("desempenho")}>
+                    <img src={Desempenho} width="16vh" />
                     <p>Desempenho</p>
+                </div>
+                <div
+                    className={`${styles.opcao} ${aba === "lancar" ? styles.ativo : ""}`}
+                    onClick={() => clicar("lancar")}>
+                    <img src={Lancar} width="16vh" />
+                    <p>Lançar Notas</p>
+                </div>
+                <div
+                    className={`${styles.opcao} ${aba === "recado" ? styles.ativo : ""}`}
+                    onClick={() => clicar("recado")}>
+                    <img src={Recado} width="24vh" />
+                    <p>Recados</p>
                 </div>
 
             </div>
         </div>
     )
 }
+
 
 export default SideBar
