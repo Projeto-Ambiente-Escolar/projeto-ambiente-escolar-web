99ea0e97b6e5fda910b513cf6bbeca211853110a
leonardolinsz <leonardo.furlanis@germinare.org.br>
2026-02-26 16:49:04 -0300

    adicionando proteção de rotas

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+import Cookies from "js-cookie";
+
+function PrivateRoute({ children, allowedRoles }) {
+  const usuarioCookie = Cookies.get("usuario");
+
+  if (!usuarioCookie) {
+    return <Navigate to="/acesso-negado" replace />;
+  }
+
+  const usuario = JSON.parse(usuarioCookie);
+
+  if (allowedRoles && !allowedRoles.includes(usuario.tipo)) {
+    return <Navigate to="/acesso-negado" replace />;
+  }
+
+  return children;
+}
+
+export default PrivateRoute;
